f8224dcd2f9ebdfdb632c88b18cef1932f373e66
Tamanna <thisistamanna.03@gmail.com>
2025-09-13 23:57:39 +0530

    added pincode

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -2,9 +2,10 @@
 import { useTranslation } from "react-i18next"
 import i18n from "./../../i18n"
 import { signOut } from "next-auth/react"
-import { useRef } from "react"
+import { useRef, useState } from "react"
 
 export default function Home() {
+  const [pin,setPin] = useState(true);
   const { t } = useTranslation()
   const changeLanguage = (lang) => {
     i18n.changeLanguage(lang)
@@ -36,30 +37,28 @@ const handleSubmit= async()=>{
   });
   const data = await res.json();
   console.log(data);
+  setPin(false);
 }
   return (
     <div>
-        <div className="flex float-right">
-        <button onClick={()=>signOut({callbackUrl:'/'})} className="p-3 rounded-lg font-bold">SignOut</button>
       <div className="mb-4">
-        <button onClick={() => changeLanguage("en")} className="px-3 py-1 border mr-2">
-          English
-        </button>
-        <button onClick={() => changeLanguage("hi")} className="px-3 py-1 border">
-          हिंदी
-        </button>
+        <button onClick={()=>signOut({callbackUrl:'/'})} className="px-3 py-1 border-2 border-purple-600 rounded-lg font-bold mr-2">SignOut</button>
+        <button onClick={() => changeLanguage("en")} className="px-3 py-1 border-2 border-purple-600 ro rounded-lg font-bold mr-2">English</button>
+        <button onClick={() => changeLanguage("hi")} className="px-3 py-1 border-2 border-purple-600 ro rounded-lg font-bold mr-2">हिंदी</button>
       </div>
-      </div>
-
-      <h2 className="text-xl font-bold text-center m-4">{t("pincode")}</h2>
+      {pin && <div className="flex justify-center">
+      <div className="border-2 border-purple-600 bg-gradient-to-r from-purple-600 to-black w-[50%]">
+      <h2 className="text-xl font-bold text-center m-4 text-white">{t("pincode")}</h2>
       <div className="flex justify-center align-middle">
         {Array.from({ length: 6 }, (_, i) => (
           <input key={i} type="text" maxLength={1} ref={inputRefs[i]} onChange={(e) => handleChange(e, i)}
-            className="w-12 p-3 m-2 rounded-lg text-white font-bold bg-purple-600 text-center"
+            className="w-12 p-3 m-2 rounded-lg text-white font-bold bg-purple-600 text-center border-2 border-white"
           />
         ))}
-        <div><button onClick={handleSubmit}>{t("done")}</button></div>
+        </div>
+        <div><button onClick={handleSubmit} className="bg-white text-black rounded-lg shadow-lg p-2 m-5 flex justify-center items-center">{t("done")}</button></div>
       </div>
+      </div>}
     </div>
   )
 }
